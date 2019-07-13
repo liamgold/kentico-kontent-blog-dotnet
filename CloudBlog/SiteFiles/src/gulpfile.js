1f240514f85925dd52360e72348582636737bf8c
Liam Goldfinch <liamwoody323@gmail.com>
2019-07-13 01:19:03 +0100

    removed gulp and updated dependencies

--- CloudBlog/SiteFiles/src/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var requireDir = require('require-dir');
-
-requireDir('./tasks');
-
-gulp.task('build', ['build:webpack']);
-gulp.task('serve', ['serve:default']);
-gulp.task('default', ['serve']);
